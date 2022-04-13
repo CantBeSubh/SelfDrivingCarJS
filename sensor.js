2e694c5c9a3410a5b94112fae3ea4b0f3e543aad
HolyCow <subhranshupati0412@gmail.com>
2022-04-13 14:07:10 +0530

    senor

--- sensor.js
+++ sensor.js
@@ -1,15 +1,47 @@
 class Sensor{
     constructor(car){
         this.car=car
-        this.rayCnt=3
+        this.rayCnt=10
         this.rayLen=100
-        this.raySpread=Math.Pi/4
+        this.raySpread=2*Math.PI
 
         this.rays=[]
+        this.#castRays()
+
+        this.readings=[]
     }
     
-    update(){
+    update(roadBorders){
         this.#castRays()
+        this.readings=[]
+        for(let i=0;i<this.rays.length;i++){
+            this.readings.push(
+                this.#getReading(this.rays[i],roadBorders)
+            )
+        }
+    }
+
+    #getReading(ray,roadBorders){
+      let touches=[]
+    //   console.log(roadBorders)
+      for(let i=0;i<roadBorders.length;i++){
+          const touch=getIntersection(
+            ray[0],
+            ray[1],
+            roadBorders[i][0],
+            roadBorders[i][1]
+          )
+          if(touch){
+            touches.push(touch)
+          }
+      }
+      if(touches.length==0){
+          return null
+      }else{
+          const offsets=touches.map(elm=>elm.offset)
+          const minOffset=Math.min(...offsets)
+          return touches.find(elm=>elm.offset==minOffset)
+      }
     }
 
     #castRays(){
@@ -19,7 +51,7 @@ class Sensor{
                 this.raySpread/2,
                 -this.raySpread/2,
                 this.rayCnt==1?0.5:i/(this.rayCnt-1)
-            )+this.car.angle
+            )+this.car.angle||this.car.angle
             const start={x:this.car.x,y:this.car.y}
             const end={
                 x:this.car.x-Math.sin(rayAngle)*this.rayLen,
@@ -31,6 +63,10 @@ class Sensor{
     
     draw(ctx){
         for(let i=0;i<this.rayCnt;i++){
+            let end=this.rays[i][1]
+            if(this.readings[i]){
+                end=this.readings[i]
+            }
             ctx.beginPath()
             ctx.innerWidth=2
             ctx.strokeStyle='yellow'
@@ -38,9 +74,23 @@ class Sensor{
                 this.rays[i][0].x,
                 this.rays[i][0].y
             )
-            ctx.lineTo(
+            ctx.lineTo( 
+                end.x,
+                end.y
+            )
+            ctx.stroke()
+        
+
+            ctx.beginPath()
+            ctx.innerWidth=2
+            ctx.strokeStyle='black'
+            ctx.moveTo(
                 this.rays[i][1].x,
                 this.rays[i][1].y
+            )
+            ctx.lineTo( 
+                end.x,
+                end.y
             )
             ctx.stroke()
         }
